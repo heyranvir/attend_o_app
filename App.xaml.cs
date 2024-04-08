86c31cc9f981bb6e80f9313d9f95939c146ce075
Ranvir Singh <124471296+heyranvir@users.noreply.github.com>
2024-04-08 12:29:12 -0400

    uploading files

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace LoginFlowMAUI;
+
+public partial class App : Application
+{
+	public App()
+	{
+
+		Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NBaF5cXmZCe0xyWmFZfVpgc19GYFZQRGY/P1ZhSXxXdkFjXX5XcHRURGBZUEw=");
+
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,33 @@
+﻿using LoginFlowMAUI.Pages;
+using LoginFlowMAUI.Services;
+using Microsoft.Extensions.Logging;
+using Syncfusion.Maui.Core.Hosting;
+
+namespace LoginFlowMAUI;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder.ConfigureSyncfusionCore();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		builder.Services.AddTransient<AuthService>();
+		builder.Services.AddTransient<LoadingPage>();
+		builder.Services.AddTransient<LoginPage>();
+		builder.Services.AddTransient<ProfilePage>();
+
+		return builder.Build();
+	}
+}
